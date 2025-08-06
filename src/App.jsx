886afba98d63fa5b7a2637d184edf9e26a8bc751
kalamazaad <abdulkalamazaad2447@gmail.com>
2025-08-06 09:07:36 +0530

    v8

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 class App extends Component {
   render() {
+    const BASEURL = import.meta.env.BASE_URL;
     return (
       <>
         <header>
@@ -9,39 +10,43 @@ class App extends Component {
         </header>
         <section>
           <div className='card'>
-            <img src='1.jpeg' alt=''/>
+            <img src={`${BASEURL}1.jpeg`} alt=''/>
             <lable>Image 01</lable>
           </div>
           <div className='card'>
-            <img src='2.jpeg' alt=''/>
+            <img src={`${BASEURL}2.jpeg`} alt=''/>
             <lable>Image 02</lable>
           </div>
           <div className='card'>
-            <img src='3.jpeg' alt=''/>
+            <img src={`${BASEURL}3.jpeg`} alt=''/>
             <lable>Image 03</lable>
           </div>
           <div className='card'>
-            <img src='4.jpeg' alt=''/>
+            <img src={`${BASEURL}4.jpeg`} alt=''/>
             <lable>Image 04</lable>
           </div>
           <div className='card'>
-            <img src='5.jpeg' alt=''/>
+            <img src={`${BASEURL}5.jpeg`} alt=''/>
             <lable>Image 05</lable>
           </div>
           <div className='card'>
-            <img src='6.jpeg' alt=''/>
+            <img src={`${BASEURL}6.jpeg`} alt=''/>
             <lable>Image 06</lable>
           </div>
           <div className='card'>
-            <img src='7.jpeg' alt=''/>
+            <img src={`${BASEURL}7.jpeg`} alt=''/>
             <lable>Image 07</lable>
           </div>
           <div className='card'>
-            <img src='8.jpeg' alt=''/>
+            <img src={`${BASEURL}8.jpeg`} alt=''/>
             <lable>Image 08</lable>
           </div>
           <div className='card'>
-            <img src='9.jpeg' alt=''/>
+            <img src={`${BASEURL}9.jpeg`} alt=''/>
+            <lable>Image 09</lable>
+          </div>
+          <div className='card'>
+            <img src={`${BASEURL}10.jpeg`} alt=''/>
             <lable>Image 09</lable>
           </div>
         </section>
